e7ff793e53785fcc94390d0f82bbd167849531b4
Scott Doxey <neogeek0101@gmail.com>
2025-04-14 14:24:10 -0400

    Added empty plugin example.

---
+++ include/register_types.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_godot_cpp_plugin(ModuleInitializationLevel p_level);
+void uninitialize_godot_cpp_plugin(ModuleInitializationLevel p_level);
