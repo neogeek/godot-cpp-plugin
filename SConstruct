3e1e690b3cfb6dcb7e38a8dc9c2503be9ef74f28
Scott Doxey <neogeek0101@gmail.com>
2025-04-14 14:24:10 -0400

    Added empty plugin example.

--- SConstruct
+++ SConstruct
@@ -1,5 +1,37 @@
 #!/usr/bin/env python
 
-SConscript("godot-cpp/SConstruct")
+env = SConscript("godot-cpp/SConstruct")
+
+env.Append(CPPPATH=["include/"])
+
+sources = Glob("include/*.cpp")
+
+folder = "build/addons/GodotCppPlugin"
+
+if env["platform"] == "macos":
+    file_name = "libGodotCppPlugin.{}.{}".format(env["platform"], env["target"])
+
+    library = env.SharedLibrary(
+        "{}/{}.framework/{}".format(folder, file_name, file_name),
+        source=sources
+    )
+else:
+    library = env.SharedLibrary(
+        "{}/libGodotCppPlugin{}{}"
+            .format(folder, env["suffix"], env["SHLIBSUFFIX"]),
+        source=sources,
+    )
+
+gdextension_copy = env.Command(
+    target="{}/GodotCppPlugin.gdextension".format(folder),
+    source="GodotCppPlugin.gdextension",
+    action=Copy("$TARGET", "$SOURCE")
+)
+
+env.Depends(gdextension_copy, library)
 
 CacheDir(".scons_cache/")
+
+Default(library)
+
+Default(gdextension_copy)
---
+++ include/register_types.cpp
@@ -0,0 +1,44 @@
+#include "register_types.hpp"
+
+#include <gdextension_interface.h>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+using namespace godot;
+
+void initialize_godot_cpp_plugin(ModuleInitializationLevel p_level)
+{
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+    {
+        return;
+    }
+}
+
+void uninitialize_godot_cpp_plugin(ModuleInitializationLevel p_level)
+{
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+    {
+        return;
+    }
+}
+
+extern "C"
+{
+    auto GDE_EXPORT godot_cpp_plugin_entry(
+        GDExtensionInterfaceGetProcAddress p_get_proc_address,
+        const GDExtensionClassLibraryPtr p_library,
+        GDExtensionInitialization *r_initialization) -> GDExtensionBool
+    {
+        godot::GDExtensionBinding::InitObject init_obj(
+            p_get_proc_address, p_library, r_initialization);
+
+        init_obj.register_initializer(initialize_godot_cpp_plugin);
+        init_obj.register_terminator(uninitialize_godot_cpp_plugin);
+
+        init_obj.set_minimum_library_initialization_level(
+            MODULE_INITIALIZATION_LEVEL_SCENE);
+
+        return init_obj.init();
+    }
+}
